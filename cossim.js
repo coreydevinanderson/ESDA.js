e10a040ed4fb624d844923c12892a77bb00332ac
coreydevinanderson <coreydevinanderson@gmail.com>
2022-08-26 16:06:33 -0400

    Stylistic and convention-base edits to cossim(), standardize(), and example data.

--- cossim.js
+++ cossim.js
@@ -1,9 +1,11 @@
-// Vectorized cosine similarity function with mathjs
-// a and b are arrays to be compared
+// cossim()
+
+// Calcutes the cosine similarity for two arrays.
+
+// Utilizes math.dot() and math.norm() from math.js
+// Default norm is L2, which is what we want here.
 
 function cossim(a, b) {
-    return math.dot(a, b) / (math.norm(a) * math.norm(b))
-}
+    return math.dot(a, b) / (math.norm(a) * math.norm(b));
+  }
   
-// test
-// console.log(cossim(FIPS_standardized["_data"][0], FIPS_standardized["_data"][1]))
--- standardize.js
+++ standardize.js
@@ -1,21 +1,19 @@
-// Function to apply zscore transformation across rows of a matrix in mathjs
+// standardize()
 
+// Returns the standardized values (z-scores) for each data vector in a matrix
 
 function standardize(your_matrix) {
 
-    let standardized = math.matrix() // make empty matrix to populate
-    let your_matrix_dims = math.size(your_matrix) // get dimensions of input matrix
-    let nrow = your_matrix_dims.get([0]) // get number of rows for iteration
-     
-    for (i = 0; i < nrow; i++){         
-      let row_mean = math.mean(your_matrix["_data"][i])
-      let row_std = math.std(your_matrix["_data"][i])
-      let centered = math.add(your_matrix["_data"][i], -row_mean)
-      let z = math.divide(centered, row_std)
-      standardized["_data"][i] = z
-    }
-    
-    standardized["_size"] = your_matrix_dims["_data"] // need to do this internally
+  let your_matrix_dims = math.size(your_matrix); // get dimensions of input matrix
+  let nrow = your_matrix_dims.get([0]); // get number of rows for iteration
+  let standardized = math.zeros(your_matrix_dims["_data"]); // make matrix of zeros to replace
   
-return standardized
+  for (i = 0; i < nrow; i++){  // for each row in the matrix.
+    let row_mean = math.mean(your_matrix["_data"][i]);
+    let row_std = math.std(your_matrix["_data"][i]);
+    let centered = math.add(your_matrix["_data"][i], -row_mean); // subtract row mean from each member
+    standardized[i] = math.divide(centered, row_std);  // divide each member by the standard deviation
+  }
+  
+return standardized;
 }
